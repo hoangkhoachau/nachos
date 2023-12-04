14909f7bca8dca2a19428d04c8c96c69ca135897
hoangkhoachau <21120481@student.hcmus.edu.vn>
2023-12-04 21:25:21 +0700

    fix exception.cc

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -25,7 +25,7 @@
 #include "syscall.h"
 #include "system.h"
 #include <cstring>
-#define MAX_READ_STRING_LENGTH 255;
+#define MAX_READ_STRING_LENGTH 255
 //----------------------------------------------------------------------
 // ExceptionHandler
 // 	Entry point into the Nachos kernel.  Called when a user program
@@ -113,7 +113,7 @@ void reverse(char src[], int start, int end) {
 //Doc chuoi ki tu nhap tu ban phim:
 void ReadString(char *buffer, int length)
 {
-     synchcons->Read(buffer, length); //dung synchcons de doc du lieu tu nguoi dung
+     gSynchConsole->Read(buffer, length); //dung synchcons de doc du lieu tu nguoi dung
      int addr;
      addr = machine->ReadRegister(4); //doc thanh ghi thu 4
      System2User(addr, length, buffer); //chuyen du lieu tu system den user
@@ -124,7 +124,7 @@ void PrintString(char* buffer)
 {
      int length = 0;
      while (buffer[length] != '\0') length++; //kiem tra so luong ki tu chuoi, den ki tu \0 thi dung
-     synchcons->Write(buffer, length + 1); //in ra man hinh console
+     gSynchConsole->Write(buffer, length + 1); //in ra man hinh console
 }
 
 void ExceptionHandler(ExceptionType which) {
@@ -269,18 +269,18 @@ void ExceptionHandler(ExceptionType which) {
 	    int addr = machine->ReadRegister(4);  // doc dia chi cua chuoi
     	    int length = machine->ReadRegister(5);  // doc do dai cua chuoi
             if (length > MAX_READ_STRING_LENGTH || length <= 0) {  //kiem tra do dai cua chuoi
-                DEBUG(dbgSys, "String length exceeds " << MAX_READ_STRING_LENGTH);
-                SC_Halt();
+                /* DEBUG(dbgSys, "String length exceeds " << MAX_READ_STRING_LENGTH); */
+                interrupt->Halt();
             }
-    	    char* buffer = User2System(addr, length)
-	    ReadString(buffer, length)
+    	    char* buffer = User2System(addr, length);
+	    ReadString(buffer, length);
             delete[] buffer;
  	}
 	break;
 	case SC_PrintString: {
 	    int addr = machine->ReadRegister(4);  // doc dia chi cua chuoi
-    	    char* buffer = User2System(addr, MAX_READ_STRING_LENGTH)
-	    PrintString(buffer)
+    	    char* buffer = User2System(addr, MAX_READ_STRING_LENGTH);
+	    PrintString(buffer);
 	    AdvancePC();
             delete[] buffer;
  	}
